cbf897cb24231df51c1f2bf88f2172b0d1b1fc4e
Dinesh Narlakanti <narlakantidineshkumar@gmail.com>
2022-07-12 10:11:14 -0500

    MySql Solution
    https://leetcode.com/problems/department-highest-salary/submissions/

--- Medium/184. Department Highest Salary.sql
+++ Medium/184. Department Highest Salary.sql
@@ -2,5 +2,4 @@
 select d.name as Department, e.name as Employee, e.salary as Salary 
 from Employee e join Department d on e.departmentId = d.id
 group by e.departmentId, e.name
-having salary IN (select max(salary) from Employee group by departmentId);
-
+having (salary, departmentId) In (select max(salary), departmentId from Employee group by departmentId );
